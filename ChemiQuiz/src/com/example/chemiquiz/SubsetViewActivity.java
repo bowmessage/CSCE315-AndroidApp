5412914e5268fdf2058d4269ed484337f3f9c83b
jbowman <jbowman@L111C01.cs.tamu.edu>
2013-11-11 16:45:04 -0600

    Added plus button menu functionality to push new activity.

--- ChemiQuiz/src/com/example/chemiquiz/SubsetViewActivity.java
+++ ChemiQuiz/src/com/example/chemiquiz/SubsetViewActivity.java
@@ -1,8 +1,11 @@
 package com.example.chemiquiz;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class SubsetViewActivity extends Activity {
 
@@ -10,6 +13,9 @@ public class SubsetViewActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subset_view);
+        
+        
+        
     }
 
 
@@ -18,6 +24,17 @@ public class SubsetViewActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.subset_view, menu);
         return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_add:
+            	startActivity(new Intent(this, SearchViewActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
     
 }
