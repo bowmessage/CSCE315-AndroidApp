c785fc6a73a972c247d93c00cdf2a9781fb95d1a
jbowman <jbowman@L111H04.cs.tamu.edu>
2013-11-18 16:47:36 -0600

    Added some tests.

---
+++ ChemiQuizTest/src/com/example/chemiquiz/test/SearchViewTest.java
@@ -0,0 +1,60 @@
+package com.example.chemiquiz.test;
+
+import junit.framework.Assert;
+import android.content.Context;
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+
+import com.example.chemiquiz.ChemicalSubset;
+import com.example.chemiquiz.R;
+import com.example.chemiquiz.SearchViewActivity;
+
+public class SearchViewTest extends ActivityUnitTestCase<SearchViewActivity> {
+		
+	public SearchViewTest(){
+		super(SearchViewActivity.class);
+	}
+	
+	Context mContext;
+
+    public void setUp(){
+        try {
+            super.setUp();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mContext = this.getInstrumentation().getContext();
+    }   
+
+    public void testListViewExists(){
+    	Intent searchIntent = new Intent(mContext, SearchViewActivity.class);
+    	searchIntent.putExtra("com.exmaple.chemiquiz.AddingToSubsetID", 0);
+    	startActivity(searchIntent, null, null);
+    	Assert.assertNotNull(this.getActivity().findViewById(R.id.searchResults));
+    }
+    
+    public void testOneResultFromAspirinSearch(){
+    	Intent searchIntent = new Intent(mContext, SearchViewActivity.class);
+    	searchIntent.putExtra("com.exmaple.chemiquiz.AddingToSubsetID", 0);
+    	SearchViewActivity a = startActivity(searchIntent, null, null);
+    	
+    	EditText query = (EditText) a.findViewById(R.id.searchText);
+    	query.setText("aspirin");
+    	
+    	ImageButton searchButton = (ImageButton) a.findViewById(R.id.searchButton);
+    	searchButton.performClick();
+    	
+    	ListView l = (ListView) a.findViewById(R.id.searchResults);
+    	Assert.assertEquals(l.getCount(), 1);
+    }
+	
+	public SearchViewTest(Class<SearchViewActivity> activityClass) {
+		super(activityClass);
+	}
+	
+	
+}
--- ChemiQuizTest/src/com/example/chemiquiz/test/SubsetViewTest.java
+++ ChemiQuizTest/src/com/example/chemiquiz/test/SubsetViewTest.java
@@ -4,18 +4,15 @@ import junit.framework.Assert;
 import android.content.Context;
 import android.content.Intent;
 import android.test.ActivityUnitTestCase;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
+import com.example.chemiquiz.ChemicalSubset;
 import com.example.chemiquiz.R;
 import com.example.chemiquiz.SubsetViewActivity;
 
 public class SubsetViewTest extends ActivityUnitTestCase<SubsetViewActivity> {
-	
-	/*@Override
-	public void TestCase(){
-		Activity mActivity = getActivity();
-		assertTrue(mActivity.getActionBar().isShowing());
-	}*/
-	
+		
 	public SubsetViewTest(){
 		super(SubsetViewActivity.class);
 	}
@@ -31,9 +28,17 @@ public class SubsetViewTest extends ActivityUnitTestCase<SubsetViewActivity> {
         mContext = this.getInstrumentation().getContext();
     }   
 
-    public void test(){
+    public void testListViewExists(){
     	startActivity(new Intent(mContext, SubsetViewActivity.class), null, null);
     	Assert.assertNotNull(this.getActivity().findViewById(R.id.subsetViewsubsetList));
+    }
+    
+    public void testListViewTiedToSubsetArrayList(){
+    	SubsetViewActivity a = startActivity(new Intent(mContext, SubsetViewActivity.class), null, null);
+    	SubsetViewActivity.subsets.add(new ChemicalSubset("test1"));
+    	ListView l = (ListView) a.findViewById(R.id.subsetViewsubsetList);
+    	((BaseAdapter) l.getAdapter()).notifyDataSetChanged();
+    	Assert.assertEquals(l.getCount(), 1);
     }
 	
 	public SubsetViewTest(Class<SubsetViewActivity> activityClass) {
