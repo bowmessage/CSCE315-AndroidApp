32898daa6e21fb1a3373c67da0d458baf33a641b
jbowman <jbowman@L111H04.cs.tamu.edu>
2013-11-18 16:46:48 -0600

    Updated Search View for better selections

--- ChemiQuiz/src/com/example/chemiquiz/SearchViewActivity.java
+++ ChemiQuiz/src/com/example/chemiquiz/SearchViewActivity.java
@@ -20,14 +20,19 @@ import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -48,21 +53,58 @@ public class SearchViewActivity extends Activity {
         
         resultantChemSpiderIDs = new ArrayList<Integer>();
         
+        
+        Bundle extras = getIntent().getExtras();
+        final int curChemicalSubsetID = extras.getInt("com.exmaple.chemiquiz.AddingToSubsetID");
+        
+        
         final EditText searchVal = (EditText) findViewById(R.id.searchText);
         ImageButton searchButton = (ImageButton) findViewById(R.id.searchButton);
-        Button addButton = (Button) findViewById(R.id.addButton);
+        final Button addButton = (Button) findViewById(R.id.addButton);
         searchResults = (ListView) findViewById(R.id.searchResults);
+        
+        addButton.setEnabled(false);
         
         final ResultsArrayAdapter adapter = new ResultsArrayAdapter(this,
             	android.R.layout.simple_list_item_1, resultantChemSpiderIDs);
             
+        searchResults.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         searchResults.setAdapter(adapter);
+        searchResults.setOnItemClickListener(new OnItemClickListener(){
+
+			@Override
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				CheckBox c = (CheckBox) view.findViewById(R.id.checkBox);
+				c.toggle();
+				view.setSelected(!view.isSelected());
+				
+				if(searchResults.getCheckedItemCount() > 0){
+					addButton.setEnabled(true);
+				} else addButton.setEnabled(false);
+			}
+        	
+        });
         
         searchButton.setOnClickListener(new OnClickListener(){
 			@Override
 			public void onClick(View v) {
 				String query = searchVal.getEditableText().toString();
 				new XmlParsing("http://www.chemspider.com/search.asmx/SimpleSearch?query="+query+"&token=f52ab236-347f-41dd-973d-a0e6668b7e14").executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, new String[]{null});
+			}
+        });
+        
+        addButton.setOnClickListener(new OnClickListener(){
+			@Override
+			public void onClick(View v) {
+				ArrayList<Integer> chemSpiderIDsSelected = new ArrayList<Integer>();
+				SparseBooleanArray valuesSelected = searchResults.getCheckedItemPositions();
+		        for (int i = 0; i < valuesSelected.size(); i++) {
+		            if(valuesSelected.valueAt(i) == true) {
+		            	SubsetViewActivity.subsets.get(curChemicalSubsetID).add(adapter.getItem(i));
+		            	//finish();
+		            	//TODO FIX THIS
+		            }
+		        }
 			}
         });
         
@@ -104,43 +146,7 @@ public class SearchViewActivity extends Activity {
                 resultantChemSpiderIDs.clear();
 
                 for (int i = 0; i < nodeList.getLength(); i++) {
-                	
                 	resultantChemSpiderIDs.add(Integer.parseInt(nodeList.item(i).getTextContent()));
-
-                    /*Node node = nodeList.item(i);
-                    Element fstElmnt = (Element) node;
-                    NodeList idList = fstElmnt.getElementsByTagName("int");
-                    Element nameElement = (Element) nameList.item(0);
-                    nameList = nameElement.getChildNodes();
-                    title.add(""+ ((Node) nameList.item(0)).getNodeValue());
-
-                    System.out.println("name : "+((Node) nameList.item(0)).getNodeValue());
-
-
-                    Element fstElmnt1 = (Element) node;
-                    NodeList nameList1 = fstElmnt1.getElementsByTagName("id");
-                    Element nameElement1 = (Element) nameList1.item(0);
-                    nameList1 = nameElement1.getChildNodes();
-                    description.add(""+ ((Node) nameList1.item(0)).getNodeValue());
-
-                    System.out.println("id : "+ ((Node) nameList1.item(0)).getNodeValue());
-
-                    Element fstElmnt2 = (Element) node;
-                    NodeList nameList2 = fstElmnt2.getElementsByTagName("cost");
-                    Element nameElement2 = (Element) nameList2.item(0);
-                    nameList2 = nameElement2.getChildNodes();
-                    id.add(""+ ((Node) nameList2.item(0)).getNodeValue());
-
-                    System.out.println("cost : "+ ((Node) nameList2.item(0)).getNodeValue());
-
-                    Element fstElmnt3 = (Element) node;
-                    NodeList nameList3 = fstElmnt3.getElementsByTagName("description");
-                    Element nameElement3 = (Element) nameList3.item(0);
-                    nameList3 = nameElement3.getChildNodes();
-                    cost.add(""+ ((Node) nameList3.item(0)).getNodeValue());
-
-                    System.out.println("description : "+ ((Node) nameList3.item(0)).getNodeValue());*/
-
                 }
 
             } catch (MalformedURLException e) {
